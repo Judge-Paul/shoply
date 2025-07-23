60e9f751632817889da867569de0a8dff9f5b630
Judge-Paul <judgepaulogebe3@gmail.com>
2025-07-23 16:15:34 +0100

    chore: minor setup

---
+++ app/_layout.tsx
@@ -0,0 +1,28 @@
+import { Tabs } from "expo-router";
+import { Home, ShoppingCart } from "lucide-react-native";
+
+export default function TabLayout() {
+	return (
+		<Tabs
+			screenOptions={{
+				tabBarActiveTintColor: "black",
+				tabBarInactiveTintColor: "gray",
+			}}
+		>
+			<Tabs.Screen
+				name="index"
+				options={{
+					title: "",
+					tabBarIcon: ({ color }) => <Home color={color} />,
+				}}
+			/>
+			<Tabs.Screen
+				name="cart"
+				options={{
+					title: "",
+					tabBarIcon: ({ color }) => <ShoppingCart color={color} />,
+				}}
+			/>
+		</Tabs>
+	);
+}
